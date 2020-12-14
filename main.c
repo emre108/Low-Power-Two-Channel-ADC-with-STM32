ce4ef20e5c2d32a57eaf83dd1feddeb772e5ec69
Emre <48506415+emre108@users.noreply.github.com>
2020-12-14 22:12:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -29,11 +29,49 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
+typedef enum{
+	
+	reset=0x00,
+	set=0xff,
+	
+}Bool;
 
+typedef enum{
+	
+	eTimerID1=0x01,
+	eTimerID2,
+	eTimerID3,
+	
+	eTimerIDMax=0xff,
+	
+}eTimerID_t;
+
+typedef struct{
+	
+	Bool isRunning;
+	volatile Bool timeout;
+	unsigned long setTime;
+	unsigned long Time;
+}stTimer_t;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define TIMER_COUNT 4
+
+static stTimer_t SoftTimer[TIMER_COUNT];
+
+
+Bool ConverterState=reset;
+Bool stop_bit_flag=reset;
+uint8_t ADCvalues[2];
+uint8_t StreamedValues[2];
+uint8_t i=0;
+uint16_t delayCounter;
+Bool exti_flag=reset;
+Bool SoftTimer1IRQ_Flag=reset;
+char rx_buf[1];
+char tx_buf[50];
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -45,22 +83,10 @@
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
+RTC_HandleTypeDef hrtc;
+
 UART_HandleTypeDef huart6;
-typedef enum{
-	
-	reset=0x00,
-	set=0xff,
-}Bool;
 
-Bool ConverterState=reset;
-uint8_t ADCvalues[2];
-uint8_t StreamedValues[2];
-uint8_t i=0;
-uint16_t delayCounter;
-Bool exti_flag=reset;
-
-char rx_buf[1];
-char tx_buf[50];
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -71,12 +97,119 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_USART6_UART_Init(void);
+static void MX_RTC_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+	void SoftTimer_ISR(void){
+		for(uint8_t i=0; i<TIMER_COUNT; i++){
+			if(SoftTimer[i].isRunning==set){
+				if(SoftTimer[i].setTime <= SoftTimer[i].Time){
+					SoftTimer[i].timeout=set;
+				}
+				else {
+					SoftTimer[i].Time++;
+				}	
+			}
+			else {
+				SoftTimer[i].timeout=reset;
+				SoftTimer[i].Time=0;
+			}
+		}
+	}
+	
+	void SoftTimer_Init(eTimerID_t TimerID, unsigned long time){
+		SoftTimer[TimerID].Time=0;
+		SoftTimer[TimerID].isRunning=set;
+		SoftTimer[TimerID].setTime=time;
+		SoftTimer[TimerID].timeout=reset;
+	}
+	
+	Bool SoftTimer_GetStatus(eTimerID_t TimerID){
+		return (SoftTimer[TimerID].isRunning);
+	}
+
+	void SoftTimer_Reset(eTimerID_t TimerID){
+		SoftTimer[TimerID].Time=0;
+		SoftTimer[TimerID].isRunning=set;
+		SoftTimer[TimerID].timeout=reset;
+	}
+
+	
+	void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+	if(SoftTimer1IRQ_Flag==reset){
+	SoftTimer_ISR();
+	}
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+void RTC_WKUP_IRQHandler(void)
+{
+  /* USER CODE BEGIN RTC_WKUP_IRQn 0 */
+	HAL_ResumeTick();
+	SystemClock_Config();
+	char *str6="Wake up with RTC internal\r\n";
+	HAL_UART_Transmit(&huart6,(uint8_t*)str6,strlen(str6),HAL_MAX_DELAY);
+  /* USER CODE END RTC_WKUP_IRQn 0 */
+  HAL_RTCEx_WakeUpTimerIRQHandler(&hrtc);
+  /* USER CODE BEGIN RTC_WKUP_IRQn 1 */
+
+  /* USER CODE END RTC_WKUP_IRQn 1 */
+}
+
+void EXTI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI2_IRQn 0 */
+	if(ConverterState==reset && exti_flag==reset){
+		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_RESET);
+		rx_buf[0]='o';
+		exti_flag=set;
+	}
+	else if(ConverterState==set && exti_flag==set){
+		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_RESET);
+		rx_buf[0]='c';
+		exti_flag=reset;
+	}
+  /* USER CODE END EXTI2_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
+  /* USER CODE BEGIN EXTI2_IRQn 1 */
+
+  /* USER CODE END EXTI2_IRQn 1 */
+}
+void EXTI0_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI0_IRQn 0 */
+	HAL_ResumeTick();
+	SystemClock_Config();
+	char *str5="Wake up with EXTI-0\r\n";
+	HAL_UART_Transmit(&huart6,(uint8_t*)str5,strlen(str5),HAL_MAX_DELAY);
+  /* USER CODE END EXTI0_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+  /* USER CODE BEGIN EXTI0_IRQn 1 */
+
+  /* USER CODE END EXTI0_IRQn 1 */
+}
+	
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+		if(hadc->Instance==ADC1){
+			for (i=0; i<2; i++){
+				StreamedValues[i]=ADCvalues[i];
+			}
+		}
+	}
+
 
 /* USER CODE END 0 */
 
@@ -84,8 +217,6 @@ static void MX_USART6_UART_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-
-
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -113,17 +244,52 @@ int main(void)
   MX_DMA_Init();
   MX_ADC1_Init();
   MX_USART6_UART_Init();
+  MX_RTC_Init();
   /* USER CODE BEGIN 2 */
+	SoftTimer_Init(eTimerID1,100); // 250ms
+	SoftTimer_Init(eTimerID2,500); // 100ms
+	
+	for(uint8_t i=0; i<20; i++){
+		HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15|GPIO_PIN_14|GPIO_PIN_13|GPIO_PIN_12);
+		while(SoftTimer[eTimerID1].timeout != set);
+		SoftTimer_Reset(eTimerID1);
+	}
+	SoftTimer1IRQ_Flag=set;
+	///RTC Wakeup Interrupt Generation: Wakeup Time Base = (RTC_WAKEUPCLOCK_RTCCLK_DIV /(LSE or LSI))
+	if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc,0x4E20 , RTC_WAKEUPCLOCK_RTCCLK_DIV16) != HAL_OK) //WakeUpCounter = Wakeup Time / Wakeup Time Base
+  {
+		/*RTC_WAKEUPCLOCK_RTCCLK_DIV = RTCCLK_Div16 = 16 
+		Wakeup Time Base = 16 /(32KHz) = ~0,0005s
+		Wakeup Time = 10s 
+		WakeUpCounter = 10s/0,0005s = 20000 = 0x4E20*/
+    Error_Handler();
+  }			
+	
+	char *str0="The Board is on Stop Mode\r\n To wake up the device please press the blue button\r\n";
+	HAL_UART_Transmit(&huart6,(uint8_t*)str0,strlen(str0),HAL_MAX_DELAY);
+	HAL_SuspendTick();
+	HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON,PWR_SLEEPENTRY_WFI); //Stop mode is activated
+	
+	HAL_RTCEx_DeactivateWakeUpTimer(&hrtc); //deactivate the wake up interrupt by RTC to prevent periodic wake up
+	
 	HAL_ADC_Start_DMA(&hadc1,(uint32_t*)ADCvalues,2);
+	char *str="Press O to open\r\n Press C to close\r\n";
+	HAL_UART_Transmit(&huart6,(uint8_t*)str,strlen(str),HAL_MAX_DELAY);
+	char *str1="To control the stream manually, press the black button\r\n";
+	HAL_UART_Transmit(&huart6,(uint8_t*)str1,strlen(str1),HAL_MAX_DELAY);
+	
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {	
+		
 		sprintf(tx_buf,"streaming:%02d:%02d\r\n",StreamedValues[0],StreamedValues[1]);
-		HAL_UART_Receive(&huart6,(uint8_t*)rx_buf,50,1000);
-		if(rx_buf[0]=='o' && ConverterState==reset){
+		HAL_UART_Receive(&huart6,(uint8_t*)rx_buf,1,1000);
+
+    if(rx_buf[0]=='o' && ConverterState==reset){
 			HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15 |	GPIO_PIN_13 ,GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_RESET);
 			ConverterState=set;
@@ -131,12 +297,13 @@ int main(void)
 		}
 		else if(rx_buf[0]=='c' && ConverterState==set){
 			HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15 | GPIO_PIN_13 | GPIO_PIN_14,GPIO_PIN_RESET);
-			HAL_UART_Transmit(&huart6,"STOP\r\n",4,100);
+			char *str4="Stream STOPPED\r\n";
+			HAL_UART_Transmit(&huart6,(uint8_t*)str4,strlen(str4),HAL_MAX_DELAY);
 			ConverterState=reset;
 			exti_flag=reset;
 		}
 		else if(ConverterState==set){
-			HAL_UART_Transmit(&huart6,(uint8_t*)tx_buf,strlen(tx_buf),1000);
+			HAL_UART_Transmit(&huart6,(uint8_t*)tx_buf,strlen(tx_buf),HAL_MAX_DELAY);
 		}
 		
     /* USER CODE END WHILE */
@@ -154,6 +321,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage 
   */
@@ -161,8 +329,9 @@ void SystemClock_Config(void)
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
   /** Initializes the CPU, AHB and APB busses clocks 
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 8;
@@ -183,6 +352,12 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+  PeriphClkInitStruct.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
@@ -243,6 +418,74 @@ static void MX_ADC1_Init(void)
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+  /** Initialize RTC Only 
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN Check_RTC_BKUP */
+    
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date 
+  */
+  sTime.Hours = 13;
+  sTime.Minutes = 25;
+  sTime.Seconds = 38;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sDate.WeekDay = RTC_WEEKDAY_SUNDAY;
+  sDate.Month = RTC_MONTH_DECEMBER;
+  sDate.Date = 1;
+  sDate.Year = 20;
+
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Enable the WakeUp 
+  */
+  /*if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 0, RTC_WAKEUPCLOCK_RTCCLK_DIV16) != HAL_OK)
+  {
+    Error_Handler();
+  } */
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
 
 }
 
@@ -313,6 +556,12 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin : PC2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
   /*Configure GPIO pin : PA0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
@@ -330,55 +579,12 @@ static void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
+  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+
 }
 
 /* USER CODE BEGIN 4 */
-	void SysTick_Handler(void)
-{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
-	if(delayCounter>0){
-		delayCounter--;
-	}
-  /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
-
-  /* USER CODE END SysTick_IRQn 1 */
-}
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
-		if(hadc->Instance==ADC1){
-			for (i=0; i<2; i++){
-				StreamedValues[i]=ADCvalues[i];
-			}
-		}
-	}
-
-void ms_delay(uint16_t delay){ //in case if delay needed. 
-	delayCounter=delay;
-	while(delay);
-}
-
-void EXTI0_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI0_IRQn 0 */
-	if(ConverterState==reset && exti_flag==reset){
-		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_RESET);
-		rx_buf[0]='o';
-		exti_flag=set;
-	}
-	else if(ConverterState==set && exti_flag==set){
-		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_RESET);
-		rx_buf[0]='c';
-		exti_flag=reset;
-	}
-  /* USER CODE END EXTI0_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
-  /* USER CODE BEGIN EXTI0_IRQn 1 */
-
-  /* USER CODE END EXTI0_IRQn 1 */
-}
 
 /* USER CODE END 4 */
 
